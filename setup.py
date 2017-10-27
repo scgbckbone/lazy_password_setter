5a83cd7fa65c32e58219f7bc9f4b9fe152e18b1a
scag <virgovica@gmail.com>
2017-10-27 19:07:21 +0200

    Started package creation.

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+import setuptools
+
+
+def read_(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name), "r") as f:
+        lng_description = f.read()
+    return lng_description
+
+
+setuptools.setup(
+    name='lazyPWDsetter',
+    version='0.0.1',
+    url="https://github.com/scgbckbone/lazy_password_setter",
+    description='Resetting password on remote hosts via ssh.',
+    license="BSD",
+    long_description=read_('README'),
+    author='Andrej Virgovic',
+    author_email='virgovica@gmail.com',
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License'
+    ],
+    packages=[
+        'lazy_password_setter',
+        'lazy_password_setter/config',
+        'lazy_password_setter/docs',
+        'lazy_password_setter/log',
+        'lazy_password_setter/src',
+        'lazy_password_setter/tests',
+    ],
+    install_requires=[
+        'pykeepass',
+        'fabric'
+    ],
+)
